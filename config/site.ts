5c53b8c287af6682ff61007b99c6a3264110554a
Nishiv Singh <singh.136@iitj.ac.in>
2023-08-12 00:18:17 +0530

    Initial commit

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Shopo",
+  description: "Best place to find latest fashions at reasonable price",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
