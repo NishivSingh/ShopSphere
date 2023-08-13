287b8f2548ff4f423b4056ef71648e172ff5b5c9
Nishiv Singh <singh.136@iitj.ac.in>
2023-08-13 23:22:36 +0530

    Added the functionality to filter products

--- app/page.tsx
+++ app/page.tsx
@@ -13,16 +13,25 @@ interface Props {
   searchParams: {
     date?: string
     price?: string
+    color?: string
+    category?: string
+    size?: string
   }
 }
 
 export default async function Page({ searchParams }: Props) {
-  const { date = "desc", price } = searchParams
+  const { date = "desc", price, color, category, size } = searchParams
   const priceOrder = price ? `| order(price ${price})` : ""
   const dateOrder = date ? `| order(_createdAt ${date})` : ""
   const order = `${priceOrder}${dateOrder}`
+
+  const productFilter = `_type == "product"`
+  const categoryFilter = category ? `&& "${category}" in categories` : ""
+  const colorFilter = color ? `&& "${color}" in colors` : ""
+  const sizeFilter = size ? `&& "${size}" in sizes` : ""
+  const filter = `*[${productFilter}${categoryFilter}${sizeFilter}${colorFilter}]`
   const products = await client.fetch<SanityProduct[]>(
-    groq`*[_type == "product"] ${order}{
+    groq`${filter} ${order}{
       _id,
       _createdAt,
       name,
